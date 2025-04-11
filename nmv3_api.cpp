2be643a1d43b43cfa5f50a286cad3beae02e76e4
bobjoe400 <cnamattern@gmail.com>
2025-04-10 21:25:23 -0700

    getting closer I think :)

--- nmv3_api.cpp
+++ nmv3_api.cpp
@@ -50,7 +50,7 @@ void broadcast(HardwareSerial connection, char *data, uint8_t bytes) {
 
     if(debug){
         Serial.printf("Sending Broadcast Command Packet:\r\n");
-        printFullPacket((uint8_t*) &pkt, pkt_size);
+        printPacketContents((uint8_t*) &pkt, pkt_size);
     }
 
     connection.write((uint8_t *) &pkt, pkt_size);
@@ -126,7 +126,7 @@ void parse_ping_packet(RangeDataResponsePacket_t* rangeResponse, DeviceAction_t*
 
 void packet_received_modem(uint8_t* packetBuffer, uint8_t size, DeviceAction_t* da) {
     if (debug) {
-        printFullPacket((uint8_t*) packetBuffer, size);
+        printPacketContents((uint8_t*) packetBuffer, size);
     }
     
     if (size < 1) {
