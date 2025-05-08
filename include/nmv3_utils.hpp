92dc2a9809cfe91537b8bb025fb72fcd7323a069
EchoNet-Capstone <echonetcapstone@gmail.com>
2025-05-08 16:55:26 -0700

    finished with the silo

--- include/nmv3_utils.hpp
+++ include/nmv3_utils.hpp
@@ -2,17 +2,61 @@
 
 #include <stdint.h>
 
-int
+#ifdef ARDUINO // ARDUINO
+#include <Arduino.h>
+
+#ifdef min // min
+#undef min
+#endif //min
+
+#ifdef max //min
+#undef max
+#endif //min
+
+#endif // ARDUINO
+
+// ----- Helper functions -----
+
+static int
 fieldToInt(
     char* field,
     uint8_t field_len
-);
+){
+    char temp[field_len + 1] = {0};
 
-void
+    memcpy(temp, field, field_len);
+    temp[field_len] = '\0';
+
+    return atoi(temp);
+}
+
+static void
 printBufferContents(
     uint8_t* buf,
-    uint8_t buf_size
-);
+    uint8_t size
+){
+    if (size == 0) {
+        Serial.printf("\tOops! This buffer is empty!\r\n"); 
+        return;
+    }
+
+    Serial.printf("\tBuffer Contents (%03u bytes): \r\n", size);
+    Serial.printf("\t    |     0          1          2          3          4          5          6          7\r\n");
+
+    int i = 0;
+    for (; i < size - 1; i++) {
+        if (i % 8 == 0)
+            Serial.printf("\r\n\t%2i |", i / 8);
+
+        Serial.printf("%c (%03u), ", (char)buf[i], buf[i]);
+    }
+
+    // Final byte — no trailing comma
+    if (i % 8 == 0)
+        Serial.printf("\r\n\t%2i |", i / 8);
+
+    Serial.printf("%c (%03u)\r\n", (char)buf[i], buf[i]);
+}
 
 static inline
 uint16_t
